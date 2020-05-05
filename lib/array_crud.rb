d85110159fa32fe78bb7d7bb1b82bf41127f10e9
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-05 16:10:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,19 +3,19 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  ['Kamrin', 29, 'Aries', 150]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop()
 end
 
 def remove_element_from_start_of_array(array)
